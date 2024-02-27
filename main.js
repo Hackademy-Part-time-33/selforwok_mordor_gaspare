98d9cff37faed15d07c18129217df3c3ab8a9a79
gaspare d'antone <dantogas@gmail.com>
2024-02-27 17:58:35 +0100

    selforwrmordor

---
+++ main.js
@@ -0,0 +1,62 @@
+
+
+// Traccia 1 
+
+let num1 = 5
+let num2 = 8
+let num3 = 12
+let num4 = 6
+let tot = (num1 + num2 + num3 + num4);
+let media = (tot / 4)
+
+
+
+console.log('la somma tra i numeri è ' + tot + 'e la media è' + media);
+
+// traccia 2
+
+let currentyears = 2024;
+let bdayears = 1946;
+let years = (currentyears - bdayears);
+let top100 = (100 - years);
+
+console.log('hai ' + years + 'ti mancancano ' + top100 + 'per raggiungere 100' );
+
+//  traccia3
+
+let numero1 = parseInt (prompt ('inserisci un numero'));
+let numero2 = parseInt (prompt ('inserisci il secondo numero'));
+let somma = (numero1 + numero2);
+let sottrazione = (numero1 - numero2);
+let moltiplicazione = (numero1 * numero2);
+let divisione = (numero1 / numero2);
+let potenzanm1 = (numero1 **2);
+let potenzanm2 = (numero2 **2);
+
+console.log('con i numeri da te scelti,i risultati delle varie operazioni sono '  + ' somma ' + somma + ' sottrazione ' + sottrazione + ' moltiplicazione ' + moltiplicazione + ' divisione ' + divisione + ' potenza del nm1 ' + potenzanm1 + ' potenza del nm2 ' + potenzanm2);
+
+// traccia4
+
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+
+  console.log (array_1[0][0],array_1 [1][0],array_1 [0][1],array_2 [4][0][0]  );
+  console.log(array_1[0][0],array_1 [1][0],array_1 [0][1],array_2 [0][0][0]);
+  console.log(array_1[0][0],array_1 [1][0],array_1 [0][1],array_1 [2][0],array_1 [2][1],array_2 [3][0],array_2 [4][1],array_1 [0][2]);
+
+
+
+
+
